540ee2085f34e27edf4f29d7aac21dbba05c3444
Chaithrika <chaithrikareddy7@gmail.com>
2026-04-18 21:15:25 +0530

    Update to 8b for the project to know more specific name

--- ai_backend.py
+++ ai_backend.py
@@ -11,11 +11,11 @@ client = genai.Client(api_key=api_key)
 
 # 3. Create the function
 def get_ai_response(prompt):
-    # Using the much newer and faster Gemini 2.0 Flash model!
+    # Using the much older and faster Gemini 1.58b Flash model! for quick responses. For more complex tasks, we can switch to the 8b or 16b models.
     response = client.models.generate_content(
-    model='gemini-1.5-flash',
-    contents=prompt
-)
+        model='gemini-1.5-flash-8b',
+        contents=prompt
+    )
     return response.text
 
 # 4. A quick test to make sure it works!
